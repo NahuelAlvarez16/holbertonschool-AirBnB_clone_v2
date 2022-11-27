22b2ae8afa980bfea2ff06b4a8594ba4ff071a28
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-27 19:46:56 -0300

    Update state and city classes.

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+from models import storage
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all,delete", backref="states")
+
+    @property
+    def cities(self):
+        """
+        Return a list of all
+        City objects that have a state_id attribute equal to the id attribute of the
+        current State object.
+        """
+        cities = []
+        for city in storage.all(City):
+            if self.id == city.state_id:
+                cities.append(city)
+        return cities
