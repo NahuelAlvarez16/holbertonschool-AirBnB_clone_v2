a711556691daed91d20f15558729c100788c763b
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-28 00:37:30 -0300

    Fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Script that starts a Flask web application:
+    Script that starts a Flask web application
 """
 from flask import Flask, request
 from markupsafe import escape
@@ -9,6 +9,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """
+    It returns the string `Hello HBNB!`
+    """
     return f'Hello HBNB!'
 
 app.run(host='0.0.0.0', port=5000)
