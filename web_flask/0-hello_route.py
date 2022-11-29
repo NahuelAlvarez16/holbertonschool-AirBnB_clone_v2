7dd4570a94eaab0c7ef1f8a8ffeb783cce44f3ce
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-29 13:58:47 -0300

    Fixes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application """
 
+
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    It returns the string `Hello HBNB!`
+    """
+    return f'Hello HBNB!'
+
 if __name__ == "__main__":
-    from flask import Flask, request
-    from markupsafe import escape
-
-
-    app = Flask(__name__)
-
-    @app.route('/', strict_slashes=False)
-    def hello():
-        """
-        It returns the string `Hello HBNB!`
-        """
-        return f'Hello HBNB!'
     app.run(host='0.0.0.0', port=5000)
